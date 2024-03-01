044ce8a8e75fae5298052c9ba49a137c0ca185b8
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-01 09:46:31 +0300

    Update TicketForm props

--- app/TicketPage/page.jsx
+++ app/TicketPage/page.jsx
@@ -2,7 +2,7 @@ import TicketForm from '../(components)/TicketForm';
 const TicketPage = async () => {
   return (
     <div>
-      <TicketForm />
+      <TicketForm oldData={''} />
     </div>
   );
 };
