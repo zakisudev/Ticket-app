5d12ae3de882faf0401e5a78d0bae29e5aca3e6c
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-01 09:35:22 +0300

    add delete server component

---
+++ app/(components)/DeleteBlock.jsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouter } from 'next/navigation';
+
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+  const deleteTicket = async () => {
+    try {
+      const res = await fetch(`/api/tickets/${id}`, {
+        method: 'DELETE',
+      });
+
+      if (res.status === 200) {
+        router.refresh();
+      } else {
+        console.log('Error');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <FontAwesomeIcon
+      icon={faX}
+      className="text-red-400 hover:cursor-pointer hover:text-red-200"
+      onClick={deleteTicket}
+    />
+  );
+};
+export default DeleteBlock;
