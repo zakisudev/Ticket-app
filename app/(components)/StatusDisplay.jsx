c26e24ea29ee0c5aa31f5d2c2ae0ab8ab9d7041d
zakisudev@gmail.com <zakisu250@gmail.com>
2024-03-01 09:34:05 +0300

    add separate Status component

---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,28 @@
+const StatusDisplay = ({ status }) => {
+  const getStatus = (status) => {
+    let color = 'bg-gray-400';
+    switch (status) {
+      case 'in-progress':
+        color = 'bg-yellow-400';
+        return color;
+      case 'not started':
+        color = 'bg-red-400';
+        return color;
+      case 'completed':
+        color = 'bg-green-400';
+        return color;
+      default:
+        return color;
+    }
+  };
+  return (
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getStatus(
+        status
+      )}`}
+    >
+      {status}
+    </span>
+  );
+};
+export default StatusDisplay;
